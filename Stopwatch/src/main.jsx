9399ca38ec002e0cd6b084ba28407f00a567083c
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-09 20:19:52 -0500

    setting up stopwatch documents

---
+++ Stopwatch/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Stopwatch from './assets/Components/Stopwatch'
+import './style.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
