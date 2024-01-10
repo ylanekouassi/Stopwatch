7734d55161f3864cc179645445ca5b1ef4d6ea6d
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-10 18:39:43 -0500

    stopwatch styled & buttons created

--- Stopwatch/src/assets/Components/Stopwatch.jsx
+++ Stopwatch/src/assets/Components/Stopwatch.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 export default function Stopwatch() {
   return (
-    <div>
-        <div>
-            <span>minutes</span>
-            <span>seconds</span>
+  <div>
+        <div className='Container'>
+            <div>
+                <span>00</span><br />
+                <span>minutes</span>
+            </div>     
+            <div>  
+                <span>00</span><br /> 
+                <span>seconds</span>
+            </div>
         </div>
-        <div>
-            <button></button>
-            <button></button>
-            <button></button>
+        <div className='Buttons'>
+                <button>Start</button>
+                <button>Pause</button>
+                <button>Stop</button>
         </div>
-    </div>
+    </div>  
   )
 }
