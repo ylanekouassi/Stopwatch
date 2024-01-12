a70bb55d1059a1d0c953352feee4fa42345feaac
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-11 21:45:05 -0500

    start button is working

--- Stopwatch/src/assets/Components/Stopwatch.jsx
+++ Stopwatch/src/assets/Components/Stopwatch.jsx
@@ -2,26 +2,44 @@ import React, {useRef} from 'react'
 import { useState, useEffect } from 'react'
 
 export default function Stopwatch() {
+
+    const [time, setTime] = useState({seconds: 0, minutes: 0});
+
+    
+    let id = useRef();
+
+    function handleTime(){
+        id.current = setInterval(() => {
+    
+            setTime((previous)=>{
+    
+                if (previous.seconds === 60){
+                    return {...previous, minutes: previous.minutes+1, seconds: 0}
+                }
+    
+                return {...previous, seconds: previous.seconds + 1};
+            });
+            console.log(time);         
+        }, 1000);    
+    }
+
+
+
   return (
-
-
-
-
-
 
   <div>
         <div className='Container'>
             <div>
-                <span></span><br />
+                <span>{time.minutes}</span><br />
                 <span>minutes</span>
             </div>     
             <div>  
-                <span></span><br /> 
+                <span>{time.seconds}</span><br /> 
                 <span>seconds</span>
             </div>
         </div>
         <div className='Buttons'>
-                <button className='Start'>Start</button>
+                <button className='Start' onClick={()=>handleTime(id.current)}>Start</button>
                 <button className='Pause'>Pause</button>
                 <button className='Stop'>Stop</button>
         </div>
